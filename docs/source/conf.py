cef85fb3238d5802e5dadda1f1bb4e83dc31527f
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-08-26 09:36:36 +0100

    removed extenstion not available in read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
