400b6d364d5261ac9512d466133b864871908b2a
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-10-14 13:23:58 +0100

    upped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='opentargets',
-      version='1.2.0a1',
+      version='1.2.0b1',
       description='Client for Open Targets REST API at targetvalidation.org',
       author='Andrea Pierleoni',
       author_email='andreap@ebi.ac.uk',
