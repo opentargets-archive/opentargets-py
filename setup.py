e4a66c5435517099ff07ddcc3739418aa998bac5
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-11-02 15:48:34 +0000

    stricter version control for setup requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='opentargets',
-      version='1.2.0b1',
+      version='1.2.0b2',
       description='Client for Open Targets REST API at targetvalidation.org',
       author='Andrea Pierleoni',
       author_email='andreap@ebi.ac.uk',
@@ -15,10 +15,9 @@ setup(name='opentargets',
       download_url='https://github.com/CTTV/opentargets-py/tarball/1.2.0a1',
       keywords = ['opentargets', 'bioinformatics', 'python3'],
       install_requires=[
-          'requests',
-          'cachecontrol',
-          'hyper >= 0.6.2',
-          'namedtupled',
+          'requests==2.11.1',
+          'cachecontrol==0.11.6',
+          'hyper==0.7.0',
           'PyYAML'],
       extras_require={
           'tests': [
