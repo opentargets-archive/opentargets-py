a09819d1a9d92b88babb6bfb38619cf567bee0a9
Miguel C <mkarmona@gmail.com>
2017-05-25 10:03:14 +0100

    fix: pypi needs author_email field filled

--- opentargets/version.py
+++ opentargets/version.py
@@ -1,5 +1,6 @@
 __pkgname__ = 'opentargets'
 __author__ = 'OpenTargets Core Team <support@targetvalidation.org>'
+__author_email__ = 'support@targetvalidation.org'
 __license__ = 'Apache License, Version 2.0'
 __homepage__ = 'https://github.com/opentargets/opentargets-py'
 __version__ = '2.0.1'
